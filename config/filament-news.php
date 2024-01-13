b147dbb48ed9c1b7b24c8199e131e4d768d47fa4
Menno Tempelaar <menno@made-foryou.nl>
2024-01-13 16:49:24 +0100

    add use_main_category setting and documentation blocks

--- config/filament-news.php
+++ config/filament-news.php
@@ -2,10 +2,42 @@
 
 return [
 
+    /**
+     * ## Main category
+     *
+     * This setting adds a belongs-to relationship from a post to a
+     * category. This selected category then becomes the "main"
+     * category.
+     *
+     * @var bool
+     */
+    'use_main_category' => true,
+
+    /**
+     * ## Categories
+     *
+     * This setting creates a polymorphic many-to-many relationship
+     * between a post and categories. So it allows you to link a
+     * post to multiple categories.
+     *
+     * @var bool
+     */
     'use_categories' => true,
 
+    /**
+     * ## Database
+     *
+     * Databse settings for changing the column and table names.
+     */
     'database' => [
 
+        /**
+         * ## Database prefix
+         *
+         * This prefix will be used when creating the database tables.
+         *
+         * @var string
+         */
         'prefix' => 'made',
 
     ],
